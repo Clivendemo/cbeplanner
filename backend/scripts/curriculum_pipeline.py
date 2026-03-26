6124567d801c1af1b4e333691350b2886cb55d73
Clivendemo <mail2clive@gmail.com>
2026-03-26 16:14:16 +0300

    Fix MONGO_URI and DB_NAME formatting in curriculum_pipeline.py
    Corrected formatting of MONGO_URI and DB_NAME assignment.

--- backend/scripts/curriculum_pipeline.py
+++ backend/scripts/curriculum_pipeline.py
@@ -11,8 +11,7 @@ from datetime import datetime
 # CONFIG
 # =========================
 
-MONGO_URI = "mongodb+srv://clive_db_admin:n1ruhu5u@cbeplanner.jtshzub.mongodb.net/cbeplanner?retryWrites=true&w=majority&appName=cbeplanner
-DB_NAME	cbeplanner"
+MONGO_URI = "mongodb+srv://clive_db_admin:n1ruhu5u@cbeplanner.jtshzub.mongodb.net/cbeplanner?retryWrites=true&w=majority&appName=cbeplanner DB_NAME = "cbeplanner""
 DB_NAME = "cbeplanner"
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
